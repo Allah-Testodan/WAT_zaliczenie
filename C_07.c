10ec6d00a4c8f8d129b20a32ba7ec777f2117be5
allah_testodan <jkunickiofficial@gmail.com>
2024-01-21 22:23:48 +0100

    gonna upgrade it soon

---
+++ C_07.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void check_parity(int i) {
+    if (i > 0) {
+        printf("%d is positive! \n", i);
+    }
+        else if (i == 0) {
+            printf("%d is equal to zero! \n", i);
+    }
+    else {
+        printf("%d is negative! \n", i);
+    }
+}
+
+int check_primarity(int i) {
+    if (i <= 1) {
+        return 0;
+    }
+            for (int p = 2; p * p <= i; p++) {
+                if (i % p == 0) {
+                    return 0;
+                }  
+            }
+    return 1; 
+}
+
+int main()
+{
+    int     i;
+
+    printf("Input the value of A: \n");
+        scanf(" %d", &i);
+
+        if (check_primarity(i)) {
+            printf("%d is a prime number! \n", i);
+        }
+                else {
+                    printf("%d is not a prime number! \n", i);
+                }
+
+check_parity(i);
+check_primarity(i);
+
+return 0;
+}
