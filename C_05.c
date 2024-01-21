09a747b899d3ab3d62c3233079a536da9c4cf30c
allah_testodan <jkunickiofficial@gmail.com>
2024-01-21 19:51:27 +0100

    added C_05

---
+++ C_05.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void print_human(char h)
+{
+    write(1, &h, 1);
+}
+
+
+int main()
+{
+    char human_first_line[] = "\\o/  _o_   o\n";
+        char human_second_line[] = " |    |   /|\\\n";
+            char human_third_line[] = "/ \\  / \\  / \\";
+
+    for (int i = 0; human_first_line[i] != '\0'; i++)
+        {
+        print_human(human_first_line[i]);
+        }
+
+        for (int i = 0; human_second_line[i] != '\0'; i++)
+            {
+                print_human(human_second_line[i]);
+            }
+
+            for (int i = 0; human_third_line[i] != '\0'; i++)
+                {
+                    print_human(human_third_line[i]);
+                }
+
+    return 0;
+}
