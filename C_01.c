e1319f86bb849bbb6be207dabfbe6a57eef7a08a
allah_testodan <jkunickiofficial@gmail.com>
2024-01-21 18:51:38 +0100

    added C_01 - 03

---
+++ C_01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_text(char c)
+{
+    printf("%c", c);
+}
+
+int main()
+{
+char text[] = "Hello world!";
+
+for (int i = 0; text[i] != '\0'; i++)
+{
+    print_text(text[i]);
+}
+    return 0;
+}
---
+++ C_03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void count(int x, int y, int z, int *sum)
+{
+    *sum = x + y + z;
+}
+
+int main()
+{
+    int x, y, z, sum;
+
+    printf("Input the value of first input: \n");
+    scanf("%d", &x);
+
+    printf("Input the value of second input: \n");
+    scanf("%d", &y);
+
+    printf("Input the value of third input: \n");
+    scanf("%d", &z);
+
+    count(x, y, z, &sum);
+    printf("The sum is equal to: %d", sum);
+    return 0;
+}
