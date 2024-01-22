d6e444778d97a408cceb07f544362d10f226bf95
allah_testodan <jkunickiofficial@gmail.com>
2024-01-22 09:51:35 +0100

    added C_08

---
+++ C_08.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main()
+{
+int     i,  j;
+
+    printf("Input the value of A: \n");
+        if ((scanf(" %d", &i)) != 1) {
+            printf("Invalid input, enter a valid integer value: \n");
+                return 1;
+        }
+
+    printf("Input the value of B: \n");
+        if ((scanf(" %d", &j)) != 1) {
+            printf("Invalid input, enter a valid integer value: \n");
+                return 1;
+        }
+
+            if (j > i) {
+                printf("%d is higher than %d \n", j, i);
+            }
+                else if (j == i) {
+                    printf("%d is equal to %d \n", i, j);
+                }
+                    else {
+                        printf("%d is lower than %d \n", j, i);
+                    }
+
+    return 0;
+}
