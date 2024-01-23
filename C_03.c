08dc6116970df1234136a30dbe805bc73f059a8c
allah_testodan <jkunickiofficial@gmail.com>
2024-01-23 12:34:54 +0100

    indicator usage

--- C_03.c
+++ C_03.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-void count(int x, int y, int z, int *sum)
+void    count(int x, int y, int z, int *sum)
 {
     *sum = x + y + z;
 }
 
-int main()
+int     main()
 {
     int x, y, z, sum;
 
     printf("Input the value of first input: \n");
-    scanf("%d", &x);
+        scanf("%d", &x);
 
     printf("Input the value of second input: \n");
-    scanf("%d", &y);
+        scanf("%d", &y);
 
     printf("Input the value of third input: \n");
-    scanf("%d", &z);
+        scanf("%d", &z);
 
     count(x, y, z, &sum);
-    printf("The sum is equal to: %d", sum);
+        printf("The sum is equal to: %d", sum);
     return 0;
 }
