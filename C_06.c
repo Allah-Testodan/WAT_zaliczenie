05b05a2d902b8a2d0ff8aa681f2ecc54ab6cc710
allah_testodan <jkunickiofficial@gmail.com>
2024-01-23 12:37:57 +0100

    conversion between decimal/ octal/ hexadecimal

--- C_06.c
+++ C_06.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main()
+int     main()
 {
     int     a;
 
     printf("Input the value of A: \n");
-    if (scanf("%d", &a) == 1) {
-        printf("%d : %o : %x\n", a, a, a);
-        printf("%d : 0%o : 0x%x\n", a, a, a);
+        if (scanf("%d", &a) == 1) {
+            printf("%d : %o : %x\n", a, a, a);
+            printf("%d : 0%o : 0x%x\n", a, a, a);
+    
     } else {
         printf("Error: invalid input\n");
     }
