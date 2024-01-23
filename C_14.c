c936b571e582177c832075e308dec2e78fbc7555
allah_testodan <jkunickiofficial@gmail.com>
2024-01-23 10:22:17 +0100

    added C_14

---
+++ C_14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define size 10
+
+int main() 
+{
+    int array[size];
+
+for (int i = '\0'; i < size; i++) {
+    array[i] = i * i;
+}
+
+for (int i = '\0'; i < size; i++) {
+    printf("%d^2 = %d\n", i, array[i]);
+}
+
+return 0;
+}
