846f5961d0886bb25b12acfe60380845ee536489
allah_testodan <jkunickiofficial@gmail.com>
2024-01-23 12:36:27 +0100

    little man generator

--- C_05.c
+++ C_05.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void print_human(char h) {
+void    print_human(char h) {
     write(1, &h, 1);
 }
 
 
-int main()
+int     main()
 {
     char human_first_line[] = "\\o/  _o_   o\n";
         char human_second_line[] = " |    |   /|\\\n";
@@ -16,11 +16,11 @@ int main()
         }
 
         for (int i = 0; human_second_line[i] != '\0'; i++) {
-                print_human(human_second_line[i]);
+            print_human(human_second_line[i]);
             }
 
             for (int i = 0; human_third_line[i] != '\0'; i++) {
-                    print_human(human_third_line[i]);
+                print_human(human_third_line[i]);
                 }
 
     return 0;
