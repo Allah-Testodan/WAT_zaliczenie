449a929706ceef9670ee591121396439d839003e
allah_testodan <jkunickiofficial@gmail.com>
2024-01-23 12:17:58 +0100

    added C_15

---
+++ C_15.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define A 5
+#define B 2
+
+void    make_array(int array[A][B]) {
+    for (int a = 0; a < A; a++) {
+        for (int b = 0; b < B; b++) {
+            array[a][b] = a + b;
+        }
+    }
+}
+
+void  print_array(int array[A][B]) {
+    for (int a = 0; a < A; a++) {
+        for (int b = 0; b < B; b++) {
+            printf("%d + %d = %d\n", a, b, array[a][b]);
+        }
+    }
+}
+
+
+int main() {
+
+int array[A][B];
+
+make_array(array);
+print_array(array);
+
+    return 0;
+}
