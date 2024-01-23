4c97bf45165782323e7e3d501037a5841dfe5a15
allah_testodan <jkunickiofficial@gmail.com>
2024-01-23 12:33:35 +0100

    sum variables

--- C_02.c
+++ C_02.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-int count(int x, int y, int z) {
+int     count(int x, int y, int z) {
     int sum = x + y + z;
 }
 
-void print_sum(int sum) {
+void    print_sum(int sum) {
     printf("The sum is equal to: %d", sum);
 }
 
-int main()
+int     main()
 {
     int x, y, z;
 
     printf("Input the value of first input: \n");
-    scanf("%d", &x);
+        scanf("%d", &x);
 
         printf("Input the value of second input: \n");
-        scanf("%d", &y);
+            scanf("%d", &y);
 
             printf("Input the value of third input: \n");
-            scanf("%d", &z);
+                scanf("%d", &z);
 
     int result = count(x, y, z);
     print_sum(result);
